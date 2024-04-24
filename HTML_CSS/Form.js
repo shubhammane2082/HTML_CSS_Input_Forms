298e1aad7ed3c71ce57561d2a52a22fb7af11e3f
shubhammane2082 <shubhammane2082@gmail.com>
2024-04-24 15:21:28 +0530

    Apply Validation on Email

--- HTML_CSS/Form.js
+++ HTML_CSS/Form.js
@@ -4,8 +4,6 @@ const textError = document.querySelector('.text-error');
 
 let fNameregex=RegExp("^[A-Z]{1,}[a-z]{2,}$");
 
-console.log(fNameregex.test(text.value));
-
 if(fNameregex.test(text.value))
 {
     textError.textContent="";
@@ -14,4 +12,19 @@ else{
     textError.textContent="Name is Incorrect";
 }
 
+});
+
+document.addEventListener('input',function(){
+const email=document.querySelector('#email');
+const emailError=document.querySelector('.emailError');
+
+let emailRegex=RegExp("^[a-z]{1,}[.][a-z]{0,}[@][a-z]{1,}[.][a-z]{1,}[.][a-z]*$");
+
+if(emailRegex.test(email.value))
+{
+    emailError.textContent="";
+}
+else{
+    emailError.textContent="Please Enter Correct Email...";
+}
 });
